05bc6ec5dabb80e356c6dd97f55501ab22dfc2ec
Steven Van den Hout <steven.vandenhout@calibrate.be>
2013-03-07 11:22:24 +0100

    added datepicker

--- index.php
+++ index.php
@@ -1,16 +1,7 @@
-<?php
-  function save_file($file) {
-    if ($file["filefield"]["error"] == 0 && $file['filefield']['type'] == 'image/jpeg') {
-      if (move_uploaded_file($file["filefield"]["tmp_name"], "uploads/" . $file["filefield"]["name"])) return TRUE;
-    }
-    return FALSE;
-
-  }
-?>
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Upload uw foto</title>
+    <title>Diabetic</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
@@ -18,6 +9,7 @@
     <!-- Le styles -->
     <link href="css/bootstrap.css" rel="stylesheet">
     <link href="css/bootstrap-responsive.css" rel="stylesheet">
+    <link href="css/bootstrap-datetimepicker.min.css" rel="stylesheet">
     <style type="text/css">
       body {
         padding-top: 40px;
@@ -25,8 +17,8 @@
         background-color: #f5f5f5;
       }
 
-      .form-upload {
-        max-width: 175px;
+      .form-input {
+        max-width: 220px;
         padding: 19px 29px 29px;
         margin: 0 auto 20px;
         background-color: #fff;
@@ -38,12 +30,14 @@
            -moz-box-shadow: 0 1px 2px rgba(0,0,0,.05);
                 box-shadow: 0 1px 2px rgba(0,0,0,.05);
       }
-      .form-upload .form-upload-heading,
-      .form-upload .checkbox {
+      .form-input .form-input-heading,
+      .form-input .checkbox {
         margin-bottom: 10px;
       }
 
       .lead { font-size: 15px; margin-bottom: 10px; line-height: 18px; }
+
+      .input-medium-addon { width: 177px; }
 
     </style>
 
@@ -52,38 +46,39 @@
 
     <div class="container">
 
-      <form class="form-upload" action="<?php echo $_SERVER['PHP_SELF']; ?>" enctype="multipart/form-data" method="post">
-        <h3 class="form-upload-heading">Print uw foto</h3>
+      <form class="form-input" action="<?php echo $_SERVER['PHP_SELF']; ?>" enctype="multipart/form-data" method="post">
+        <h3 class="form-input-heading">Suikerwaarde</h3>
         <?php
-          // save the image
-          if (is_array($_FILES) && isset($_FILES['filefield'])) {
-            if (save_file($_FILES)) {
-              echo '<p class="lead text-success">Uw foto werd naar de printer verstuurd.<p>';
-            }
-            else {
-              echo '<p class="lead text-error">Er liep iets fout bij het uploaden van uw foto.<p>';
-            }
-          }
+          //echo '<p class="lead text-success">Uw waarden werden bewaard.<p>';
         ?>
-        <div class="control-group"><div class="controls">
-          <input type="file" id='filefield' name='filefield'>
-        </div></div>
-        <button class="btn btn-medium btn-success btn-block" type="submit">Upload</button>
+        <input type="number" class="input-block-level" id='suiker' name='suiker' placeholder="Suikerwaarde" />
+
+        <div id="datetimepicker" class="input-prepend date">
+          <span class="add-on"><i data-time-icon="icon-time" data-date-icon="icon-calendar"></i></span>
+          <input data-format="dd/MM/yyyy hh:mm:ss" type="text" class='input-medium-addon' placeholder="Tijdstip"></input>
+        </div>
+
+        <input type="number" class="input-block-level" id='novorapid' name='novorapid' placeholder="Eenheden Novorapid" />
+        <input type="number" class="input-block-level" id='lantus' name='lantus' placeholder="Eenheden Lantus" />
+        <textarea class="input-block-level" id='eten' name='eten' placeholder="Wat ga je eten?"></textarea>
+
+        <button class="btn btn-large btn-block btn-primary" type="submit">Opslaan</button>
       </form>
 
     </div> <!-- /container -->
 
     <script src="http://code.jquery.com/jquery.js"></script>
     <script src="js/bootstrap.min.js"></script>
-    <script src="js/bootstrap-filestyle.js"></script>
+    <script src="js/bootstrap-datetimepicker.min.js"></script>
     <script type="text/javascript">
-      $('#filefield').filestyle({
-        buttonText: 'Foto',
-        classButton: 'btn-primary',
-        icon: true,
-        classIcon: 'icon-picture icon-white',
-        classText: 'input-small'
+      $(function() {
+        $('#datetimepicker').datetimepicker({
+          language: 'be-NL',
+          maskInput: true,
+          pickTime: false,
+        });
       });
     </script>
+
   </body>
 </html>
